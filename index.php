b8885abfae4d91ea787ba494004d0a629badf3f4
Faqih Ardiansyah <faqihbackup2024@gmail.com>
2025-05-04 11:03:04 +0700

    create a php code

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $name = "Faqih Ardiansyah";
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Halaman Awal</title>
+    </head>
+    <body>
+        <h1>Halo nama saya <?php echo $name ?></h1> 
+    </body>
+    </html>
